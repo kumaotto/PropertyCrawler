729c6856b00cae370cb027492c8ee4bf5900ca31
kumaotto <kwkomaki.kom@gmail.com>
2024-08-13 21:49:15 +0900

    🧱 cronを1日1回に変更

--- cdk/cdk/crawling_batch_stack.py
+++ cdk/cdk/crawling_batch_stack.py
@@ -77,7 +77,7 @@ class CrawlingBatchStack(Stack):
         '''
         rule = events.Rule(
             self, 'DailyCrawlingRule',
-            schedule=events.Schedule.cron(minute='0', hour='9,21')  # 午前9時と午後9時に実行
+            schedule=events.Schedule.cron(minute='0', hour='9')  # 午前9時と午後9時に実行
         )
 
         # EventBridgeルールにLambdaをターゲットとして設定
