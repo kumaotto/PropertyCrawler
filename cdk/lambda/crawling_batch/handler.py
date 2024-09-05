21a138da710e591aecbcd7b315b18435d7e92651
kumaotto <kwkomaki.kom@gmail.com>
2024-09-06 01:14:02 +0900

    👽️ URL更新

--- cdk/lambda/crawling_batch/handler.py
+++ cdk/lambda/crawling_batch/handler.py
@@ -6,7 +6,7 @@ from scraper import scrape_properties
 
 def lambda_handler(event, context):
 
-    initial_url = 'https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&ta=13&bs=040&ekInput=20110&tj=60&nk=-1&ct=14.0&cb=0.0&co=1&md=07&md=08&md=09&md=10&kz=1&ts=1&ts=3&et=10&mt=9999999&mb=0&cn=20&tc=0401303&tc=0400101&tc=0400103&tc=0400301&tc=0400901&tc=0400905&shkr1=03&shkr2=03&shkr3=03&shkr4=03&fw2=&pc=30'
+    initial_url = event.get('url')
     properties = scrape_properties(initial_url)
     new_properties = []
 
